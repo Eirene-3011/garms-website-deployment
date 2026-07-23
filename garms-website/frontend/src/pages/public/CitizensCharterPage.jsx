5c68da23ea0071233ed171ab4b5a2f90ff3469ea
Railey Andrei Acosta <raileyacosta0824@gmail.com>
2026-07-23 14:39:26 +0800

    Update CitizensCharterPage.jsx

--- garms-website/frontend/src/pages/public/CitizensCharterPage.jsx
+++ garms-website/frontend/src/pages/public/CitizensCharterPage.jsx
@@ -19,7 +19,6 @@ export default function CitizensCharterPage() {
     <div>
       <div className="page-header">
         <div className="container">
-          <div className="page-header-icon"><IconScroll size={26} /></div>
           <h1>Citizen's Charter</h1>
           <p>Our commitment to efficient, transparent, and accountable public service</p>
         </div>
